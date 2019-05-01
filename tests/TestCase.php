2a46fc70c466b20734abab265af5e7185acf19f7
Orkhan Ahmadov <ahmadov90@gmail.com>
2019-05-01 16:34:24 +0400

    Init

---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orkhanahmadov\CBARCurrencyParser\Tests;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Resolve application aliases.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+
+        ];
+    }
+}
---
+++ tests/Unit/ParserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Orkhanahmadov\CBARCurrencyParser\Tests\Unit;
+
+use Orkhanahmadov\CBARCurrencyParser\Tests\TestCase;
+
+class ParserTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
