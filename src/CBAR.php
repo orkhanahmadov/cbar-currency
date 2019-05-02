a225a9d139ac2df6d1eed9ffcaf7788085aae98b
Orkhan Ahmadov <ahmadov90@gmail.com>
2019-05-02 16:40:35 +0400

    WIP

--- src/CBAR.php
+++ src/CBAR.php
@@ -8,13 +8,17 @@ use Orkhanahmadov\CBARCurrency\Exceptions\CurrencyException;
 class CBAR
 {
     /**
+     * @var string|null
+     */
+    private $date;
+    /**
      * @var Client
      */
     private $client;
     /**
      * @var array
      */
-    private $currencies = [];
+    private $rates = [];
     /**
      * @var float|int|null
      */
@@ -22,26 +26,39 @@ class CBAR
 
     /**
      * Parser constructor.
+     * @param string|null $date
      */
-    public function __construct()
+    public function __construct(?string $date = null)
     {
+        // todo: validate date
+        $this->date = $date;
         $this->client = new Client();
     }
 
     public function for(string $date)
+    {
+        // todo: validate date
+        $this->date = $date;
+
+        if (!isset($this->rates[$date])) {
+            $this->getRatesFromCBAR($date);
+        }
+
+        return $this;
+    }
+
+    private function getRatesFromCBAR(string $date)
     {
         $response = $this->client->get('https://www.cbar.az/currencies/'.$date.'.xml');
 
         $xml = simplexml_load_string($response->getBody()->getContents());
 
         foreach ($xml->ValType[1]->Valute as $currency) {
-            $this->currencies[(string) $currency->attributes()['Code']] = [
+            $this->rates[$date][(string) $currency->attributes()['Code']] = [
                 'rate' => (float) $currency->Value,
                 'nominal' => (int) $currency->Nominal
             ];
         }
-
-        return $this;
     }
 
     /**
@@ -51,17 +68,19 @@ class CBAR
      */
     public function __get(string $currency)
     {
-        if (!isset($this->currencies[$currency])) {
+        // todo: validate if date is available. if not, fetch new
+
+        if (!isset($this->rates[$this->date][$currency])) {
             throw new CurrencyException('Currency with '.$currency.' code is not available');
         }
 
         if ($this->aznAmount) {
-            $conversion = bcdiv($this->aznAmount, $this->currencies[$currency]['rate'], 4);
+            $conversion = bcdiv($this->aznAmount, $this->rates[$this->date][$currency]['rate'], 4);
             $this->aznAmount = null;
             return $conversion;
         }
 
-        return bcdiv($this->currencies[$currency]['rate'], $this->currencies[$currency]['nominal'], 4);
+        return bcdiv($this->rates[$this->date][$currency]['rate'], $this->rates[$this->date][$currency]['nominal'], 4);
     }
 
     /**
@@ -72,7 +91,9 @@ class CBAR
      */
     public function __call(string $currency, array $arguments)
     {
-        if (!isset($this->currencies[$currency])) {
+        // todo: validate if date is available. if not, fetch new
+
+        if (!isset($this->rates[$this->date][$currency])) {
             throw new CurrencyException('Currency with '.$currency.' code is not available');
         }
 
@@ -99,18 +120,18 @@ class CBAR
     }
 
     /**
-     * @param array $currencies
+     * @param array $rates
      */
-    public function setCurrencies(array $currencies): void
+    public function setRates(array $rates): void
     {
-        $this->currencies = $currencies;
+        $this->rates = $rates;
     }
 
     /**
      * @return array
      */
-    public function getCurrencies(): array
+    public function getRates(): array
     {
-        return $this->currencies;
+        return $this->rates;
     }
 }
--- tests/Unit/CBARTest.php
+++ tests/Unit/CBARTest.php
@@ -25,23 +25,46 @@ class CBARTest extends TestCase
         $this->cbar->setClient($this->guzzler->getClient());
     }
 
+//    public function test_throws_exception_when_invalid_date_is_given()
+//    {
+//        $cbar = new CBAR('40.10.2019');
+//    }
+
     public function test_for_sets_currencies_from_cbar()
     {
         $this->guzzler
             ->expects($this->once())
             ->get('https://www.cbar.az/currencies/01.05.2019.xml')
             ->willRespond(new Response(200, [], file_get_contents(__DIR__.'/../dummy_response.xml')));
-        $this->assertEmpty($this->cbar->getCurrencies());
+        $this->assertEmpty($this->cbar->getRates());
 
         $this->cbar->for('01.05.2019');
 
-        $this->assertNotEmpty($this->cbar->getCurrencies());
+        $this->assertNotEmpty($this->cbar->getRates());
+    }
+
+    public function test_guzzle_will_not_fetch_cbar_api_if_rates_for_given_date_are_already_available()
+    {
+        $this->guzzler
+            ->expects($this->once())
+            ->get('https://www.cbar.az/currencies/01.05.2019.xml')
+            ->willRespond(new Response(200, [], file_get_contents(__DIR__.'/../dummy_response.xml')));
+
+        $this->cbar->for('01.05.2019');
+        $this->cbar->for('01.05.2019');
     }
 
     public function test_magic_get_method_returns_currency_rate()
     {
-        $cbar = new CBAR();
-        $cbar->setCurrencies(['USD' => ['nominal' => 1, 'rate' => 1.7053]]);
+        $cbar = new CBAR('01.05.2019');
+        $cbar->setRates([
+            '01.05.2019' => [
+                'USD' => [
+                    'nominal' => 1,
+                    'rate' => 1.7053
+                ]
+            ]
+        ]);
 
         $this->assertEquals(1.7053, $cbar->USD);
     }
@@ -51,15 +74,29 @@ class CBARTest extends TestCase
         $this->expectException(CurrencyException::class);
         $this->expectExceptionMessage('Currency with EUR code is not available');
         $cbar = new CBAR();
-        $cbar->setCurrencies(['USD' => ['nominal' => 1, 'rate' => 1.7053]]);
+        $cbar->setRates([
+            '01.05.2019' => [
+                'USD' => [
+                    'nominal' => 1,
+                    'rate' => 1.7053
+                ]
+            ]
+        ]);
 
         $cbar->EUR;
     }
 
     public function test_magic_set_method_returns_calculated_amount()
     {
-        $cbar = new CBAR();
-        $cbar->setCurrencies(['USD' => ['nominal' => 1, 'rate' => 1.7053]]);
+        $cbar = new CBAR('01.05.2019');
+        $cbar->setRates([
+            '01.05.2019' => [
+                'USD' => [
+                    'nominal' => 1,
+                    'rate' => 1.7053
+                ]
+            ]
+        ]);
 
         $this->assertEquals(170.53, $cbar->USD(100));
     }
@@ -69,15 +106,29 @@ class CBARTest extends TestCase
         $this->expectException(CurrencyException::class);
         $this->expectExceptionMessage('Currency with EUR code is not available');
         $cbar = new CBAR();
-        $cbar->setCurrencies(['USD' => ['nominal' => 1, 'rate' => 1.7053]]);
+        $cbar->setRates([
+            '01.05.2019' => [
+                'USD' => [
+                    'nominal' => 1,
+                    'rate' => 1.7053
+                ]
+            ]
+        ]);
 
         $cbar->EUR(100);
     }
 
     public function test_azn_method_returns_azn_to_other_currency_conversion_with_given_amount()
     {
-        $cbar = new CBAR();
-        $cbar->setCurrencies(['EUR' => ['nominal' => 1, 'rate' => 2]]);
+        $cbar = new CBAR('01.05.2019');
+        $cbar->setRates([
+            '01.05.2019' => [
+                'EUR' => [
+                    'nominal' => 1,
+                    'rate' => 2
+                ]
+            ]
+        ]);
 
         $this->assertEquals(0.5, $cbar->AZN()->EUR);
         $this->assertEquals(5, $cbar->AZN(10)->EUR);
